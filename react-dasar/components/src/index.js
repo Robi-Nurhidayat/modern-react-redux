e84f4734b65605827a4e4087487eef0e09a1027f
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-08-22 20:33:39 +0700

    create comment and install faker js

--- react-dasar/components/src/index.js
+++ react-dasar/components/src/index.js
@@ -1,8 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { faker } from "@faker-js/faker";
 
 const App = () => {
-  return <h1>Hai There</h1>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a className="avatar">
+          <img src={faker.image.avatar()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a className="author">Matt</a>
+          <div className="metadata">
+            <span className="date">Today at 5:42PM</span>
+          </div>
+          <div className="text">How artistic!</div>
+          {/* <div className="actions">
+            <a className="reply">Reply</a>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
