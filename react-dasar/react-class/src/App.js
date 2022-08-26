a32d1be0b1765fccc8cb0227917657de3a374076
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-08-26 13:45:19 +0700

    moving geolocation in constructor

--- react-dasar/react-class/src/App.js
+++ react-dasar/react-class/src/App.js
@@ -5,16 +5,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null };
-  }
 
-  render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
+        this.setState({ lat: position.coords.latitude });
       },
       (err) => console.log(err)
     );
-    return <div>Latitude</div>;
+  }
+
+  render() {
+    return <div>Latitude : {this.state.lat}</div>;
   }
 }
 
