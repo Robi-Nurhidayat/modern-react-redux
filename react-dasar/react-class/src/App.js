69b90987b94bbb1059c50bce2040c94dcf6bbfd6
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-08-26 13:34:15 +0700

    refactoring functional component to class component and create a object state in consructor

--- react-dasar/react-class/src/App.js
+++ react-dasar/react-class/src/App.js
@@ -1,13 +1,21 @@
+import React from "react";
 import SeasonsDisplay from "./SeasonsDisplay";
 
-function App() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-    },
-    (err) => console.log(err)
-  );
-  return <SeasonsDisplay />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
+
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+      },
+      (err) => console.log(err)
+    );
+    return <div>Latitude</div>;
+  }
 }
 
 export default App;
