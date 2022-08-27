4e9851422a2a89f82de0bd8a3115f659275dc277
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-08-27 13:02:42 +0700

    add new value is errorMessage

--- react-dasar/react-class/src/App.js
+++ react-dasar/react-class/src/App.js
@@ -4,18 +4,25 @@ import SeasonsDisplay from "./SeasonsDisplay";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render() {
-    return <div>Latitude : {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude : {this.state.lat} <br />
+        Errors : {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
