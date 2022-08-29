fcc21fd9906c6190fdfbdcb8d98d909a53cd2d22
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-08-29 20:33:25 +0700

    learn about default props

--- react-dasar/react-class/src/App.js
+++ react-dasar/react-class/src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
       return <div>Error : {this.state.errorMessage}</div>;
     }
 
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
