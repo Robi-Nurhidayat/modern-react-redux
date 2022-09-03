85d1e113eebfd710435f75ff0a5e39e4c3fed410
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-03 12:57:21 +0700

    create new state images for temp results api

--- react-dasar/pics/src/components/App.js
+++ react-dasar/pics/src/components/App.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,13 +13,15 @@ class App extends React.Component {
       },
     });
 
-    console.log(response.data.results);
-  }
+    console.log(response);
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        <h5>Found {this.state.images.length} images</h5>
       </div>
     );
   }
