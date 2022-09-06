158247dccafdf0c6a20d926f2aa82bf40320ad64
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-06 12:58:12 +0700

    create new app is videos

---
+++ react-dasar/videos/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "./Searchbar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
