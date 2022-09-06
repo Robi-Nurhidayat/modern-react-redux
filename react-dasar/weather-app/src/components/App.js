c8a24b9473cde66c3947cf2dc142204434a19dbb
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-06 13:49:36 +0700

    get data from weather app

--- react-dasar/weather-app/src/components/App.js
+++ react-dasar/weather-app/src/components/App.js
@@ -1,9 +1,24 @@
+import { data } from "autoprefixer";
+import axios from "axios";
 import React from "react";
 import Search from "./Search";
 
 class App extends React.Component {
-  onSearch = (city) => {
-    console.log(city);
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  onSearch = async (city) => {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c30a5bb4da82a5133a4d75e2554363ba`
+    );
+
+    const { main, description } = response.data.weather[0];
+
+    console.log(main);
   };
   render() {
     return (
