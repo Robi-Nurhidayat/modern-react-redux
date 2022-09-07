76e1b77e52faf1371abacb4d990cebd87d415e7f
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-07 13:54:52 +0700

    deleted key

--- react-dasar/videos/src/api/youtube.js
+++ react-dasar/videos/src/api/youtube.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAzruStatb1eynrcM5zFCN0aU1nCifUIvE";
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
