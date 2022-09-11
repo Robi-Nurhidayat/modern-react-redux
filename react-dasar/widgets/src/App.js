6d049eb65b35a76ef35bd7e3d3551253b85be71c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-11 12:47:00 +0700

    create accordion component and array of object we call items

--- react-dasar/widgets/src/App.js
+++ react-dasar/widgets/src/App.js
@@ -1,5 +1,26 @@
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "Apa itu React js",
+    content: "React adalah librari yang digunakan untuk membangun UI",
+  },
+  {
+    title: "Mengapa React",
+    content: "React merupkan librari frontend populer",
+  },
+  {
+    title: "Bagaimana cara menggunakan react",
+    content: "React dibangun dengan menggunakan prinsip component",
+  },
+];
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
 }
 
 export default App;
