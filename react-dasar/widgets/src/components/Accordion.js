44840c90c7760cbe589313e0abe1bccaddb2aed5
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-12 14:21:34 +0700

    learn abotu usestate and practice create button for number++

--- react-dasar/widgets/src/components/Accordion.js
+++ react-dasar/widgets/src/components/Accordion.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItem = items.map((item) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const ontTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount(count + 1);
+  };
+
+  const renderedItem = items.map((item, index) => {
+    const active = activeIndex === index ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className={`title ${active}`} onClick={() => ontTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`title ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItem}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItem} <h1>{activeIndex}</h1>
+      <button onClick={handleCount}>Klik</button>
+      <h1>Curren number :{count}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
