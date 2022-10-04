9540e72206118fa34b3b9d536675a1e9db4d8a2c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-04 13:13:00 +0700

    membuat reducer songs dan selected song

---
+++ react-dasar/songs/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songReducers = () => {
+  return [
+    { title: "Aku dan Kamu", duration: "2:40" },
+    { title: "Dia", duration: "3:00" },
+    { title: "Hujan", duration: "3:10" },
+    { title: "Yank", duration: "4:10" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
