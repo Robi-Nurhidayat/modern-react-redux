0442ebc976f934a4838d49a6da03ee47ea371296
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-06 17:32:29 +0700

    calling action create on songs react

--- react-dasar/songs/src/components/SongLists.js
+++ react-dasar/songs/src/components/SongLists.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongLists extends Component {
   renderedList() {
@@ -7,7 +8,12 @@ class SongLists extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -16,13 +22,13 @@ class SongLists extends Component {
   }
 
   render() {
-    console.log(this.props);
     return <div className="ui divided list">{this.renderedList()}</div>;
   }
 }
 
 const mapStateProps = (state) => {
+  console.log(state);
   return { song: state.songs };
 };
 
-export default connect(mapStateProps)(SongLists);
+export default connect(mapStateProps, { selectSong })(SongLists);
