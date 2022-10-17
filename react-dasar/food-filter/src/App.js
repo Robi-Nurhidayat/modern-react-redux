99663801e797a460f73dccf6e07cee6c2016f12d
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-17 15:27:09 +0700

    menampilkan data foods

--- react-dasar/food-filter/src/App.js
+++ react-dasar/food-filter/src/App.js
@@ -1,13 +1,51 @@
 function App() {
+  const foods = [
+    {
+      nama: "bakso",
+      harga: 12000,
+      kategori: "kuah",
+      deskripsi: "semua makanan enak ko ...",
+    },
+    {
+      nama: "Basreng",
+      harga: 1000,
+      kategori: "kering",
+      deskripsi: "semua makanan enak ko ...",
+    },
+    {
+      nama: "mie ayam",
+      harga: 8000,
+      kategori: "kuah",
+      deskripsi: "semua makanan enak ko ...",
+    },
+    {
+      nama: "martabak",
+      harga: 22000,
+      kategori: "kering",
+      deskripsi: "semua makanan enak ko ...",
+    },
+  ];
+
+  const renderFoods = foods.map((food) => {
+    return (
+      <div className="card mt-5" style={{ width: "18rem" }}>
+        <div className="card-header">{food.nama}</div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">{food.harga}</li>
+          <li className="list-group-item">{food.kategori}</li>
+          <li className="list-group-item">{food.deskripsi}</li>
+        </ul>
+      </div>
+    );
+  });
   return (
     <div className="container">
-      <div className="card mt-5" style={{ width: "18rem" }}>
-        <div className="card-header">Featured</div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-        </ul>
+      <div>{renderFoods}</div>
+      <div className="mt-5">
+        <button className="btn btn-primary m-3" onClick={() => alert()}>
+          Kering
+        </button>
+        <button className="btn btn-secondary">Kuah</button>
       </div>
     </div>
   );
