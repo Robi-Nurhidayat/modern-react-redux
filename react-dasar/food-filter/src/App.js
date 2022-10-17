419bb30b0742f6aa2994c241edd382a4f4a7ec19
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-17 12:10:22 +0700

    menambahkan bootstrap

--- react-dasar/food-filter/src/App.js
+++ react-dasar/food-filter/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card mt-5" style={{ width: "18rem" }}>
+        <div className="card-header">Featured</div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+      </div>
     </div>
   );
 }
