8fd51caf7d59117e7c82d29b267f8d1912ea2df0
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-20 12:39:32 +0700

    get data json placeholder

---
+++ react-dasar/blog-practice-1/src/components/PostList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { fetchPost } from "../actions/index";
+import { connect } from "react-redux";
+
+class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPost();
+  }
+  render() {
+    console.log(this.props.posts);
+    return <div>PostList</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPost })(PostList);
