835ddb6fda3a3d7134917680468acec8c98d74da
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-22 16:48:25 +0700

    belajar lodash _.chain()

--- react-dasar/blog/src/actions/index.js
+++ react-dasar/blog/src/actions/index.js
@@ -4,8 +4,12 @@ import _ from "lodash";
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, "userId"));
-  userIds.forEach((id) => fetchUser(id));
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => fetchUser(id));
+
+  // cara short code di atas
+
+  _.chain(getState().posts).map("userId").uniq().value();
 };
 
 export const fetchPosts = () => async (dispatch) => {
