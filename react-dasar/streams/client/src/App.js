fa8bfb75fd8dc312fd7670cc3f3085807bfd94ed
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-25 14:29:08 +0700

    belajar react router dom

--- react-dasar/streams/client/src/App.js
+++ react-dasar/streams/client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={pageOne} />
+        <Route path="/pagetwo" component={pageTwo} />
+      </BrowserRouter>
     </div>
   );
 }
 
+const pageOne = () => {
+  return <div>Page One</div>;
+};
+
+const pageTwo = () => {
+  return (
+    <div>
+      Page two <button>Click Me!</button>
+    </div>
+  );
+};
 export default App;
