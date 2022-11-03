dcbcc5bb30088bd962245a95e9805a722e5ef1e0
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-11-04 06:20:09 +0700

    belajar usereducer

--- react-dasar/acodemy/react-hook/src/App.jsx
+++ react-dasar/acodemy/react-hook/src/App.jsx
@@ -1,23 +1,47 @@
-import React, { useRef } from "react";
+import React, { useReducer, useRef } from "react";
 import { useState } from "react";
 
 const App = () => {
-  const [angka, setAngka] = useState(0);
-  const btnRef = useRef(0);
+  // const [angka, setAngka] = useState(0);
+  // const btnRef = useRef(0);
 
-  console.log("komponen di render");
-  console.log(btnRef);
+  // console.log("komponen di render");
+  // console.log(btnRef);
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setAngka(angka + 1);
+  //         console.log(btnRef.current);
+  //       }}
+  //       ref={btnRef}
+  //     >
+  //       Hello
+  //     </button>
+  //   </div>
+  // );
+
+  function reducer(state, action) {
+    if (action.type === "INCREMENT") {
+      return state + 1;
+    }
+    if (action.type === "DECREMENT") {
+      return state - 1;
+    }
+
+    return 0;
+  }
+
+  const [counter, dispatch] = useReducer(reducer, 0);
+
   return (
-    <div>
-      <button
-        onClick={() => {
-          setAngka(angka + 1);
-          console.log(btnRef.current);
-        }}
-        ref={btnRef}
-      >
-        Hello
-      </button>
+    <div style={{ textAlign: "center", maxWidth: "300px", margin: "auto" }}>
+      <h1>{counter}</h1>
+
+      <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+        <button onClick={() => dispatch({ type: "DECREMENT" })}>Kurangi</button>
+        <button onClick={() => dispatch({ type: "INCREMENT" })}>Tambah</button>
+      </div>
     </div>
   );
 };
