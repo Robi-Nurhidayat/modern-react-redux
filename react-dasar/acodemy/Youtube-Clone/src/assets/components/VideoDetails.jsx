36a8e5266d88299383e7a1ebabfc6e331ca247a3
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-11-11 09:52:52 +0700

    react player

--- react-dasar/acodemy/Youtube-Clone/src/assets/components/VideoDetails.jsx
+++ react-dasar/acodemy/Youtube-Clone/src/assets/components/VideoDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchApi } from "../../utils/fetchApi";
+import ReactPlayer from "react-player";
 
 const VideoDetails = () => {
   const { id } = useParams();
@@ -21,8 +22,26 @@ const VideoDetails = () => {
     );
   }, [id]);
 
-  console.log(videoSuggest);
-  return <div>VideoDetails</div>;
+  console.log(videoDetails);
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="grow md:basis-9/12 lg:basis-8/12">
+        <ReactPlayer
+          url={`https://www.youtube.com/watch?v=${id}`}
+          className="h-full"
+          width={"100%"}
+        />
+        <div className="p-4">
+          <h1 className="text-lg font-medium">{videoDetails?.snippet.title}</h1>
+          <p className="mb-4">{videoDetails?.statistics.viewCount} views</p>
+          <hr />
+
+          <p>{videoDetails?.snippet.description}</p>
+        </div>
+      </div>
+      <div className="md:basis-3/12 lg:basis-4/12">Video Suggest</div>
+    </div>
+  );
 };
 
 export default VideoDetails;
