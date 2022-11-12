057dd98e93ca2bd33262429732b96de5d3a515c1
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-11-13 06:07:25 +0700

    mencoba latihan menerapkan action,store,reducers,combine reducer

--- react-dasar/acodemy2/redux-app/test.js
+++ react-dasar/acodemy2/redux-app/test.js
@@ -1,29 +1,64 @@
 const redux = require("redux");
 
-const BUY_SHOES = "BUY_SHOES";
-
+const BUY_FOOD = "BUY_FOOD";
+const BUY_DRINK = "BUY_DRINK";
 const ADD_STOCK = "ADD_STOCK";
 
-const initialShoes = {
-  stok: 20,
+const STOK_PANTRY = "STOK_PANTRY";
+
+// store
+
+const restaurant = {
+  food: 50,
+  drink: 100,
+  stock: 1000,
 };
 
-const reducer = (state = initialShoes, action) => {
+const pantry = {
+  stok: 100,
+};
+
+const restaurantReducers = (state = restaurant, action) => {
   switch (action.type) {
-    case BUY_SHOES:
-      return { ...state, stok: state.stok - 2 };
+    case BUY_DRINK:
+      return { ...state, drink: state.drink - 20 };
+    case BUY_FOOD:
+      return { ...state, food: state.food - 10 };
     case ADD_STOCK:
-      return { ...state, stok: state.stok + 2 };
+      return {
+        ...state,
+        stock: state.stock + 1000,
+        food: state.food + 5,
+        drink: state.drink + 5,
+      };
     default:
       return state;
   }
 };
 
-const store = redux.createStore(reducer);
+const pantryRedcucers = (state = pantry, action) => {
+  switch (action.type) {
+    case STOK_PANTRY:
+      return {
+        ...state,
+        stok: state.stok + 100,
+      };
+    default:
+      return state;
+  }
+};
 
-store.dispatch({ type: BUY_SHOES });
-store.dispatch({ type: BUY_SHOES });
-store.dispatch({ type: BUY_SHOES });
+const combineReducers = redux.combineReducers({
+  restaurant: restaurantReducers,
+  pantry: pantryRedcucers,
+});
+
+const store = redux.createStore(combineReducers);
+
+store.dispatch({ type: BUY_DRINK });
+store.dispatch({ type: BUY_DRINK });
+store.dispatch({ type: BUY_FOOD });
 store.dispatch({ type: ADD_STOCK });
+store.dispatch({ type: STOK_PANTRY });
 
 console.log(store.getState());
