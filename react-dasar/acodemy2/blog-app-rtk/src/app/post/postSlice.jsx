9337863c72b59b39011587ceed2b40766a784c0b
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-11-19 09:52:28 +0700

    create post slice

---
+++ react-dasar/acodemy2/blog-app-rtk/src/app/post/postSlice.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: 1,
+    title: "Post 1",
+    content:
+      "ini adalah sebuah postingan yang mengandung hal yang kurang baik dalam kehidupan bernegara yatu sdaaaaaaaaaaaaaaaaa",
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    content:
+      "sdfhklhlkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk",
+  },
+];
+
+const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {},
+});
+
+export default postSlice.reducer;
