9e76cea5675b81665c6f27bdeb8f1733f34fc46c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-11-20 13:28:55 +0700

    membatasi jumlah content post yang muncul

--- react-dasar/acodemy2/blog-app-rtk/src/app/post/PostList.jsx
+++ react-dasar/acodemy2/blog-app-rtk/src/app/post/PostList.jsx
@@ -11,8 +11,8 @@ const PostList = () => {
           <div className="border rounded p-6 mb-5" key={post.id}>
             <h2 className="text-lg font-bold">{post.title}</h2>
             <p className="text-sm text-gray-500">3 menit yang lalu</p>
-            <p className="py-3">{post.content}</p>
-            <p className="text-sm">
+            <p className="py-3">{post.content.substring(0, 10)}</p>
+            <p className="text-sm text-gray-500">
               Di tulis oleh <span className="underline">Kevin</span>
             </p>
           </div>
