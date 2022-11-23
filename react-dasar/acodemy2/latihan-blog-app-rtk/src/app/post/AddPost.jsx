66bcf921f9ab43e8243b0982502ad728f60bea0d
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-11-23 07:55:06 +0700

    finishing create form add post

--- react-dasar/acodemy2/latihan-blog-app-rtk/src/app/post/AddPost.jsx
+++ react-dasar/acodemy2/latihan-blog-app-rtk/src/app/post/AddPost.jsx
@@ -4,10 +4,43 @@ const AddPost = () => {
   return (
     <div className="border p-4 mx-auto">
       <form>
-        <div className="flex flex-col">
-          <label>Title</label>
-          <input type={"text"} className="outline-none border" />
+        <div className="flex flex-col mb-2">
+          <label className="font-md text-md mb-0.5 font-medium text-gray-600">
+            Title
+          </label>
+          <input
+            type={"text"}
+            className="outline-none rounded-sm border px-2 py-1.5"
+            placeholder="Title"
+          />
         </div>
+        <div className="flex flex-col mb-2">
+          <label className="font-md text-md mb-0.5 font-medium text-gray-600">
+            Penulis
+          </label>
+          <select className="outline-none rounded-sm border px-2 py-1.5">
+            <option>--Pilih Penulis--</option>
+            <option value={"1"}>Penulis 1</option>
+            <option value={"2"}>Penulis 2</option>
+          </select>
+        </div>
+        <div className="flex flex-col">
+          <label className="font-md text-md mb-0.5 font-medium text-gray-600">
+            Content
+          </label>
+          <textarea
+            cols={30}
+            rows={10}
+            className="outline-none border px-2 py-1.5 rounded-sm"
+            placeholder="content"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="py-2 px-4 bg-blue-500 text-white font-bold rounded mt-4 w-full hover:bg-blue-600 "
+        >
+          Create Post
+        </button>
       </form>
     </div>
   );
--- react-dasar/acodemy2/latihan-blog-app-rtk/src/app/post/postSlice.jsx
+++ react-dasar/acodemy2/latihan-blog-app-rtk/src/app/post/postSlice.jsx
@@ -5,16 +5,19 @@ const initialState = [
     id: 1,
     title: "Belajar javascript",
     content: "alsfdjalsdjljasdljlasjdljasldjlasjdlajslj",
+    penulis: "Penulis 1",
   },
   {
     id: 2,
     title: "Belajar Node js",
     content: "alsfdjalsdjljasdljlasjdljasldjlasjdlajslj",
+    penulis: "Penulis 1",
   },
   {
     id: 3,
     title: "Belajar Ruby",
     content: "alsfdjalsdjljasdljlasjdljasldjlasjdlajslj",
+    penulis: "Penulis 1",
   },
 ];
 
