594f0633c7553dc7d8eec3d392380c15717dd51b
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-11-25 09:24:55 +0700

    setup pdas project in course stephen grider

---
+++ react-dasar/stephen-grider/pdas/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ProfileCard from "./components/ProfileCard";
+
+const App = () => {
+  return (
+    <div>
+      <div>Digital Personal Assistent</div>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  );
+};
+
+export default App;
