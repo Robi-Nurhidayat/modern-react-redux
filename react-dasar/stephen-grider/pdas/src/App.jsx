9b8b008c13c4a26b4986e7d6446649ebceacd3a5
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-11-27 14:31:17 +0700

    finishing pdas project - stephen grider

--- react-dasar/stephen-grider/pdas/src/App.jsx
+++ react-dasar/stephen-grider/pdas/src/App.jsx
@@ -8,10 +8,42 @@ import Siri from "./assets/images/siri.png";
 const App = () => {
   return (
     <div>
-      <div>Digital Personal Assistent</div>
-      <ProfileCard title="Alexa" handle="@Alexa" image={Alexa} />
-      <ProfileCard title="Cortana" handle="@Cortana" image={Cortana} />
-      <ProfileCard title="Siri" handle="@Siri" image={Siri} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Digital Personal Assistent</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@Alexa"
+                image={Alexa}
+                description="Alex dibuat oleh amazon. dia dapat membantu anda dalam berbelanja"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@Cortana"
+                image={Cortana}
+                description="Cortana dibuat oleh microsoft. dia dapat membantu anda dalam mengoperasikan windows"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                handle="@Siri"
+                image={Siri}
+                description="Siri dibuat oleh apple. dia dapat membantu anda dalam menemukan aplikasi terbaik di ios store"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
--- react-dasar/stephen-grider/pdas/src/components/ProfileCard.jsx
+++ react-dasar/stephen-grider/pdas/src/components/ProfileCard.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-const ProfileCard = ({ title, handle, image }) => {
+const ProfileCard = ({ title, handle, image, description }) => {
   return (
-    <div>
-      <img src={image} alt="pda logo" />
-      <div>{title}</div>
-      <div>{handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={image} alt="pdas logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{handle}</p>
+          </div>
+        </div>
+      </div>
+      <div className="content">{description}</div>
     </div>
   );
 };
