c5c350eedd94209f4edd683e7d9154c1b6108b43
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-12-01 09:49:07 +0700

    lanjutin warung yogi

--- react-dasar/project_khusus_latihan/warung-yogi/src/components/Produk.jsx
+++ react-dasar/project_khusus_latihan/warung-yogi/src/components/Produk.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 const Produk = () => {
-  return <div>Produk</div>;
+  return (
+    <div className="flex justify-between">
+      <div className="menu-category basis-4 p-4 shadow-lg ">
+        <div className="category-select flex justify-between gap-4 flex-row-reverse">
+          <label>Makanan</label>
+          <input type={"checkbox"} className="border outline-none" />
+        </div>
+        <div className="category-select flex justify-between gap-4 flex-row-reverse">
+          <label>Minuman</label>
+          <input type={"checkbox"} className="border outline-none" />
+        </div>
+        <div className="category-select flex justify-between gap-4 flex-row-reverse">
+          <label>Mandi</label>
+          <input type={"checkbox"} className="border outline-none" />
+        </div>
+      </div>
+      <div className="content basis-8 flex-1 flex items-center justify-center flex-wrap gap-4">
+        <div className="card px-4 p-2 shadow-2xl rounded">
+          <div className="header">
+            <h1>Title</h1>
+          </div>
+          <div className="body">
+            <h5>harga</h5>
+            <p>asd;kaf;smfmmmmmmmmmmm</p>
+            <button className="px-4 py-1 bg-blue-400 rounded text-white">
+              Add
+            </button>
+          </div>
+        </div>
+        <div className="card shadow-2xl">
+          <div className="header">
+            <h1>Title</h1>
+          </div>
+          <div className="body">
+            <h5>harga</h5>
+            <p>asd;kaf;smfmmmmmmmmmm</p>
+          </div>
+        </div>
+        <div className="card shadow-2xl">
+          <div className="header">
+            <h1>Title</h1>
+          </div>
+          <div className="body">
+            <h5>harga</h5>
+            <p>asd;kaf;smfmmmmmmmmm</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Produk;
