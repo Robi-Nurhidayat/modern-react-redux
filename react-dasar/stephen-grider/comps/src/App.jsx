f1e7591e9fe145510a48633548cbc5b942356f7f
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-12-14 14:34:38 +0700

    belajar accordion - setup

--- react-dasar/stephen-grider/comps/src/App.jsx
+++ react-dasar/stephen-grider/comps/src/App.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import Accordion from "./components/Accordion";
 import ButtonPages from "./pages/ButtonPages";
 
 const App = () => {
-  return <ButtonPages />;
+  const items = [
+    {
+      id: "asdsdf",
+      label: "Belajar React js",
+      content:
+        "Anda dapat belajar react js dengan siapapun.Anda dapat belajar react js dengan siapapun.Anda dapat belajar react js dengan siapapun.",
+    },
+    {
+      id: "sssssss",
+      label: "Belajar Angular js",
+      content:
+        "Anda dapat belajar react js dengan siapapun.Anda dapat belajar react js dengan siapapun.Anda dapat belajar react js dengan siapapun.",
+    },
+    {
+      id: "cccccc",
+      label: "Belajar Vue js",
+      content:
+        "Anda dapat belajar react js dengan siapapun.Anda dapat belajar react js dengan siapapun.Anda dapat belajar react js dengan siapapun.",
+    },
+  ];
+  return <Accordion items={items} />;
 };
 
 export default App;
