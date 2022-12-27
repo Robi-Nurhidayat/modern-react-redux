7ac9f2dc5fabab261918c1bd6dee714824594ca9
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-12-27 10:29:26 +0700

    create navigation context

--- react-dasar/stephen-grider/comps/src/main.jsx
+++ react-dasar/stephen-grider/comps/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { NavigationProvider } from "./context/Navigation";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <NavigationProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </NavigationProvider>
 );
