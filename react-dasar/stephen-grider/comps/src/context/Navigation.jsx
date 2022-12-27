f6c3e49e6f905da07cc4bdd2ebfad388d726da89
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-12-27 10:36:29 +0700

    window location

--- react-dasar/stephen-grider/comps/src/context/Navigation.jsx
+++ react-dasar/stephen-grider/comps/src/context/Navigation.jsx
@@ -1,10 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
+  const [currentPath, setCurrentpath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handle = () => {
+      setCurrentpath(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", handle);
+
+    return () => {
+      window.removeEventListener("popstate", handle);
+    };
+  }, []);
   return (
     <NavigationContext.Provider value={{}}>
+      {currentPath}
       {children}
     </NavigationContext.Provider>
   );
