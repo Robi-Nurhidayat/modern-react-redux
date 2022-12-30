69cb2e808b50e0546933cac303433008df198422
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-12-30 12:42:09 +0700

    asd

--- react-dasar/traversy-media/expense-tracker/src/App.jsx
+++ react-dasar/traversy-media/expense-tracker/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
+import "./App.css";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </div>
+  );
 };
 
 export default App;
