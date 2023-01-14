05301d762215a488eab18fd6890ef8ce79058910
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-01-14 09:34:55 +0700

    create Link and Route

--- react-dasar/stephen-grider/comps/src/App.jsx
+++ react-dasar/stephen-grider/comps/src/App.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+import Link from "./components/Link";
+import Route from "./components/Route";
 import AccordionPages from "./pages/AccordionPages";
 import DropdownPages from "./pages/DropdownPages";
 
 const App = () => {
-  return <DropdownPages />;
+  return (
+    <div>
+      <Link to={"/acc"}>Accordion</Link>
+      <Link to={"/drop"}>Dropdown</Link>
+
+      <div>
+        <Route path={"/acc"}>
+          <AccordionPages />
+        </Route>
+        <Route path={"/drop"}>
+          <DropdownPages />
+        </Route>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ react-dasar/stephen-grider/comps/src/components/Link.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import NavigationContext from "../context/Navigation";
+
+function Link({ to, children }) {
+  const { navigate } = useContext(NavigationContext);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    navigate(to);
+  };
+  return <a onClick={handleClick}>{children}</a>;
+}
+
+export default Link;
