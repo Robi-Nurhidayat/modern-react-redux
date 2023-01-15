2438bd2c3a1fdb6654d8833b891f2dae514bb2b6
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-01-16 06:13:16 +0700

    malanjutkan traveyuk dengan react

--- react-dasar/react-landing-page/traveyuk/src/components/header/Header.jsx
+++ react-dasar/react-landing-page/traveyuk/src/components/header/Header.jsx
@@ -2,22 +2,32 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
+  const navMenus = [
+    { label: "Home" },
+    { label: "Featured" },
+    { label: "Service" },
+    { label: "Contact Us" },
+  ];
+
+  const renderedNavMenus = navMenus.map((menu) => {
+    return (
+      <li
+        key={menu.label}
+        className="hover:text-xl cursor-pointer transition-all 0.5"
+      >
+        {menu.label}
+      </li>
+    );
+  });
   return (
     <header className="header w-full">
-      <div className="bg-header"></div>
       <div className="relative px-16 py-9 border flex justify-between items-center">
-        <div className="nav-bg"></div>
         <div>
           <span className="text-4xl text-white font-bold">Travyuk</span>
         </div>
 
         <nav>
-          <ul className="flex gap-7 text-base">
-            <li>Home</li>
-            <li>Featured</li>
-            <li>Service</li>
-            <li>Contact Us</li>
-          </ul>
+          <ul className="flex gap-7 text-base">{renderedNavMenus}</ul>
         </nav>
 
         <div className="flex">
@@ -28,8 +38,17 @@ const Header = () => {
         </div>
       </div>
 
-      <div className="hero-text-continer flex justify-center font-semibold text-2xl">
+      <div className="hero-text-continer flex justify-center font-semibold text-2xl text-gray-300 mt-40">
         <p>life is too short for a wide and beautiful world</p>
+      </div>
+
+      <div
+        className=" text-white font-bold text-center mx-auto mt-3"
+        style={{ maxWidth: "81.5rem" }}
+      >
+        <p className="text-6xl">
+          Enjoy Your Tour With Heavenly experience you've never had
+        </p>
       </div>
     </header>
   );
