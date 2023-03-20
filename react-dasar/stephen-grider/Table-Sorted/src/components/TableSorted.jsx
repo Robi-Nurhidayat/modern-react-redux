8115195395dd5b045b84f64b2b85c0b85a5f0f44
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-03-20 14:38:13 +0700

    handle Click

--- react-dasar/stephen-grider/Table-Sorted/src/components/TableSorted.jsx
+++ react-dasar/stephen-grider/Table-Sorted/src/components/TableSorted.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
 const TableSorted = (props) => {
   const { data, config } = props;
+  const [sortBy, setSortBy] = useState(null);
+  const [orderBy, setOrderBy] = useState(null);
 
+  const handleClick = (label) => {
+    if (sortBy && label !== sortBy) {
+      setOrderBy("asc");
+      setSortBy(label);
+      return;
+    }
+
+    if (orderBy === null) {
+      setOrderBy("asc");
+      setSortBy(label);
+    } else if (orderBy === "asc") {
+      setOrderBy("desc");
+      setSortBy(label);
+    } else if (orderBy === "desc") {
+      setOrderBy(null);
+      setSortBy(null);
+    }
+  };
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
       return column;
@@ -12,14 +32,22 @@ const TableSorted = (props) => {
     return {
       ...column,
       header: () => {
-        return <th className="bg-blue-500 cursor-pointer">{column.label}</th>;
+        return (
+          <th
+            onClick={() => handleClick(column.label)}
+            className="bg-blue-500 cursor-pointer"
+          >
+            {column.label}
+          </th>
+        );
       },
     };
   });
   return (
-    <>
+    <div>
+      {sortBy} - {orderBy}
       <Table {...props} config={updatedConfig} />
-    </>
+    </div>
   );
 };
 
