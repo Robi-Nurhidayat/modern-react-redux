07656f9c0b4ba86bec23ab4dd3de5d128ffc40ae
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-03-22 14:19:16 +0700

    belajar tentang konver number dari input

--- react-dasar/stephen-grider/006_-_sopt/src/pages/CounterPage.js
+++ react-dasar/stephen-grider/006_-_sopt/src/pages/CounterPage.js
@@ -1,21 +1,49 @@
 import { useState } from "react";
 import Button from "../components/Button";
+import Panel from "../components/Panel";
 // +
 
 function CounterPage({ initialCount }) {
   // const { count, increment } = useCounter(initialCount);
 
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
   };
 
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  function handleChange(e) {
+    const value = parseInt(e.target.value) || 0;
+
+    setValueToAdd(value);
+  }
+
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form className="flex flex-col gap-2">
+        <label>Add a lot!</label>
+        <input
+          type={"number"}
+          className="outline-none py-1 px-2 border w-4/12 "
+          value={valueToAdd || ""}
+          onChange={handleChange}
+        />
+        <div>
+          <Button>Add it!</Button>
+        </div>
+      </form>
+    </Panel>
   );
 }
 
