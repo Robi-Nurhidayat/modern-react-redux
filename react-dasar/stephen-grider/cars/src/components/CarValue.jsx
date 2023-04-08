d490b6f0bd53cb070a8c5cfa66559cc779f63b77
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-04-08 17:27:17 +0700

    membuat display cost

--- react-dasar/stephen-grider/cars/src/components/CarValue.jsx
+++ react-dasar/stephen-grider/cars/src/components/CarValue.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function CarValue() {
-  return <div>CarValue</div>;
+  const { data, searchTerm } = useSelector((state) => {
+    return {
+      data: state.cars.data,
+      searchTerm: state.cars.searchTerm,
+    };
+  });
+
+  let totalCost = 0;
+  data.forEach((car) => {
+    totalCost += car.cost;
+  });
+
+  console.log(totalCost);
+  if (searchTerm) {
+    totalCost = data.filter((car) => {
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+    totalCost = totalCost[0].cost;
+  }
+
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
