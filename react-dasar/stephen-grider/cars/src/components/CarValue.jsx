ca577a7d12519e5cdf4f3ae14bcac490372c0e2c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-04-09 17:39:11 +0700

    use filter metode

--- react-dasar/stephen-grider/cars/src/components/CarValue.jsx
+++ react-dasar/stephen-grider/cars/src/components/CarValue.jsx
@@ -2,26 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function CarValue() {
-  const { data, searchTerm } = useSelector((state) => {
-    return {
-      data: state.cars.data,
-      searchTerm: state.cars.searchTerm,
-    };
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    return data
+      .filter((car) => {
+        return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+      })
+      .reduce((acc, curr) => acc + curr.cost, 0);
   });
-
-  let totalCost = 0;
-  data.forEach((car) => {
-    totalCost += car.cost;
-  });
-
-  console.log(totalCost);
-  if (searchTerm) {
-    totalCost = data.filter((car) => {
-      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-
-    totalCost = totalCost[0].cost;
-  }
 
   return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
