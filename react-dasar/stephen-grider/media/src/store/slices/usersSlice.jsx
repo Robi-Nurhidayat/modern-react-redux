ed47f353e64f4565ee62df3b1b8623876d2c1ef3
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-04-11 15:09:31 +0700

    create extraReducers untuk fetchUsers

--- react-dasar/stephen-grider/media/src/store/slices/usersSlice.jsx
+++ react-dasar/stephen-grider/media/src/store/slices/usersSlice.jsx
@@ -1,13 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunk/fetchUsers";
 
 const usersSlice = createSlice({
   name: "users",
   initialState: {
-    users: [],
+    data: [],
     isLoading: false,
     error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const userReducer = usersSlice.reducer;
