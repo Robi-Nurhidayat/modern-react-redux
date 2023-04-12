c97b5fa93aa1a941bd76956e4ada7654c311a67a
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-04-12 17:39:08 +0700

    create new user

--- react-dasar/stephen-grider/media/src/components/UserLists.jsx
+++ react-dasar/stephen-grider/media/src/components/UserLists.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { addUser, fetchUsers } from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
 
 const UserLists = () => {
   const dispatch = useDispatch();
@@ -11,6 +12,10 @@ const UserLists = () => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const handleAdd = async () => {
+    await dispatch(addUser());
+  };
+
   if (isLoading) {
     return <Skeleton className={"h-10 w-full"} times={6} />;
   }
@@ -18,7 +23,25 @@ const UserLists = () => {
   if (error) {
     return <div>Error fetching data...</div>;
   }
-  return <div>{data.length}</div>;
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div className="mb-2 border rounded" key={user.id}>
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 };
 
 export default UserLists;
